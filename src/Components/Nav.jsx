1eee2d22254d7f4453ea1db7ea4437d26d6174b0
DanKhan1 <chaudriali034@gmail.com>
2024-12-19 02:39:54 +0500

    This is fourth changes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Productcontext } from '../Utils/Contex'
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  const [products] = useContext(Productcontext);
+  let particular_category = products && products.reduce((acc,currentvalue)=>[...acc,currentvalue.category],[])
+  particular_category = [...new Set(particular_category)]
+  console.log(particular_category);
+  
+  const color=()=>{
+    return `rgba(${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()},${(Math.random()*255).toFixed()},0.4)`;
+  }
+
   return (
     <>
      <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5">
@@ -12,24 +23,16 @@ const Nav = () => {
           </a>
           <hr className="my-3 w-[80%]" />
           <h1 className="text-2xl mb-3 w-[80%]">Category Filter</h1>
-          <ul className=" w-[80%] p-3 ">
-            <li className="flex items-center mb-3">
-              <span className="bg-blue-300 mr-2 rounded-full  w-[15px] h-[15px]"></span>
-              Category 1
-            </li>
-            <li className="flex items-center mb-3">
-              <span className="bg-blue-300 mr-2 rounded-full  w-[15px] h-[15px]"></span>
-              Category 2
-            </li>
-            <li className="flex items-center mb-3">
-              <span className="bg-blue-300 mr-2 rounded-full  w-[15px] h-[15px]"></span>
-              Category 3
-            </li>
-            <li className="flex items-center mb-3">
-              <span className="bg-blue-300 mr-2 rounded-full  w-[15px] h-[15px]"></span>
-              Category 4
-            </li>
-          </ul>
+          <div className=" w-[80%] p-3 ">
+            {
+              particular_category.map((specificcategory,index)=>{
+                return <Link key={index} to={`/?category=${specificcategory}`} className="flex items-center mb-3">
+                <span style={{backgroundColor: color()}} className=" mr-2 rounded-full  w-[15px] h-[15px]"></span>
+                {specificcategory}
+              </Link> 
+              })
+            }
+          </div>
         </nav>
     </>
   )
